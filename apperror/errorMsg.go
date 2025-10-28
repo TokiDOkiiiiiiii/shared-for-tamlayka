a74f46c7a313e0a0fef4a662840b1527afa72f0a
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-10-28 23:22:43 +0700

    Feat: add Marshal_failed and Send_msg_failed in error message

--- apperror/errorMsg.go
+++ apperror/errorMsg.go
@@ -2,9 +2,11 @@ package apperror
 
 import "strings"
 
-var PARSE_ERROR = "unable to parse request body"
+var PARSE_ERROR = "cannot parse request body"
 var INVALID_INPUT = "invalid request body"
-var AUTHENTHECATE_ERROR = "username or password is incorrect"
+var AUTHENTICATE_FAILED = "username or password is incorrect"
+var MARSHAL_FAILED = "failed to marshal object to JSON"
+var SEND_MSG_FAILED = "failed to send message to queue"
 
 func ServiceError(serviceName string) string {
 	return strings.ToLower(serviceName) + " service failed"
